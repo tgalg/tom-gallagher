9daec39774c287ff5672c256e870b8fc311d849b
tgalg <thomasxgallagher@gmail.com>
2023-05-18 15:49:14 -0500

    Styling the Nexus baby

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
 import '@/styles/globals.css'
 
+import Image from 'next/image'
+import profilePic from '@/../public/images/tgalg.png'
+import Nexus from '@/components/Nexus'
+import AnimatedText from '@/components/AnimatedText'
+
 export default function Home() {
-  return <main className="">Hello World</main>
+  return (
+    <>
+      <main className="flex items-center text-dark w-full min-h-screen">
+        <Nexus className="pt-0">
+          <div className="flex items-center justify-between w-full">
+            <div className="w-1/2">
+              <Image
+                src={profilePic}
+                alt="tgalg"
+                className="w-full h-auto"
+              ></Image>
+            </div>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText text="Turning Vision Into Reality With Code And Design." />
+              <p>
+                As a skilled full-stack developer, I am dedicated to turning
+                ideas into innovative web applications. Explore my latest
+                projects and articles, showcasing my expertise in React.js and
+                web development.
+              </p>
+            </div>
+          </div>
+        </Nexus>
+      </main>
+    </>
+  )
 }
---
+++ src/components/Nexus.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nexus = ({
+  children,
+  className = '',
+}: {
+  children: React.ReactNode
+  className?: string
+}) => {
+  return (
+    <div
+      className={`w-full h-full inline-block z-0 bg-light p-32 ${className}`}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Nexus
