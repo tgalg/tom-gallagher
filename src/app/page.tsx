8a43c78270944a8aba590e120d022026ff289db4
tgalg <thomasxgallagher@gmail.com>
2023-06-30 09:25:45 -0500

    tailwind typo 💨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
                 text="Tinkerer at Heart, Technologist by Trade."
                 className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
-              <p className="my-4 text-base font-mediummd:text-sm sm:text-xs">
+              <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
                 As an accomplished full-stack developer, I am committed to
                 transforming concepts into groundbreaking web applications.
                 Delve into my most recent projects, demonstrating my proficiency
